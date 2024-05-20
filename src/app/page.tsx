dfe6753e1ddcd0542f1543cb9f6ce370d4771fb5
armangohari <arman.gohari.2001@gmail.com>
2024-05-20 16:31:25 +0330

    refactor: compartmentalize page.tsx
    - Added ComingSoon.tsx component.
    - Refactored the home page.tsx component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
-import { cogGif } from "@/lib/icons";
-import Image from "next/image";
+import BackgroundMusicPlayer from "@/components/BackgroundMusicPlayer";
+import ComingSoon from "@/components/ComingSoon";
 
 export default function Home() {
   return (
-    <main className="grid h-screen w-full place-items-center">
-      {/* Loading Card Outer Container */}
-      <div className="relative grid place-items-center overflow-hidden rounded-3xl bg-black p-px">
-        {/* Loading Card Inner Container */}
-        <div className="relative z-10 flex animate-pulse items-center justify-center gap-1.5 rounded-3xl bg-black px-6 py-6">
-          {/* Icon */}
-          <Image src={cogGif} alt="cog" className="h-7 w-7" />
-          {/* Loading Text */}
-          <h2 className="text-2xl tracking-wider text-white">Coming Soon</h2>
-        </div>
-
-        {/* Circular Motion Orbs */}
-        <div className="animate-orbit bg-primary absolute rounded-full p-40 blur-3xl" />
-        <div className="animate-orbit-delayed bg-primary/75 absolute rounded-full p-40 blur-3xl" />
-      </div>
+    <main className="h-screen w-full flex justify-center items-center flex-col gap-12">
+      <ComingSoon />
+      <BackgroundMusicPlayer />
     </main>
   );
 }
---
+++ src/components/ComingSoon.tsx
@@ -0,0 +1,20 @@
+import { cogGif } from "@/lib/icons";
+import Image from "next/image";
+
+export default function ComingSoon() {
+  return (
+    <div className="relative grid place-items-center overflow-hidden rounded-3xl bg-black p-px">
+      {/* Loading Card Inner Container */}
+      <div className="relative z-10 flex animate-pulse items-center justify-center gap-1.5 rounded-3xl bg-black px-6 py-6">
+        {/* Icon */}
+        <Image src={cogGif} alt="cog" className="h-7 w-7" />
+        {/* Loading Text */}
+        <h2 className="text-2xl tracking-wider text-white">Coming Soon</h2>
+      </div>
+
+      {/* Circular Motion Orbs */}
+      <div className="absolute animate-orbit rounded-full bg-primary p-40 blur-3xl" />
+      <div className="absolute animate-orbit-delayed rounded-full bg-primary p-40 blur-3xl" />
+    </div>
+  );
+}
