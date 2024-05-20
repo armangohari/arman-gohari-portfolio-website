3070ecb2113c6cc2835d51be842bfbda4992d790
armangohari <arman.gohari.2001@gmail.com>
2024-05-20 22:13:15 +0330

    style: format the classes in /app/page.tsx with prettier
    Formatted the main tag class names with prettier-plugin-tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ComingSoon from "@/components/ComingSoon";
 
 export default function Home() {
   return (
-    <main className="h-screen w-full flex justify-center items-center flex-col gap-12">
+    <main className="flex h-screen w-full flex-col items-center justify-center gap-12">
       <ComingSoon />
       <BackgroundMusicPlayer />
     </main>
