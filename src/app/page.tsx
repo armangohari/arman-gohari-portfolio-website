96aea7bdf55880aeb3695d2e8ea98c5c7c568c7e
armangohari <arman.gohari.2001@gmail.com>
2024-06-06 21:15:52 +0330

    revert: revert back to previous home page
    + Reverted back to previous home page without screen-size fit hooks.
    + Added h-[100dvh] instead of h-screen to main.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,12 @@
-"use client";
-
 import About from "@/components/home/About";
 import Contact from "@/components/home/Contact";
 import Hero from "@/components/home/Hero";
 import Projects from "@/components/home/Projects";
 import Tools from "@/components/home/Tools";
-import { useEffect, useRef } from "react";
 
 export default function Home() {
-  // Ref to hold the main element
-  const mainRef = useRef<HTMLElement>(null);
-
-  // Effect to set the height of the main to innerHeight
-  // note: for android chrome address bar overlaying the h-screen
-  useEffect(() => {
-    const innerHeight = window.innerHeight;
-    const mainElement = mainRef.current;
-    if (mainElement) {
-      mainElement.style.height = `${innerHeight}px`;
-    }
-  }, []);
-
   return (
-    <main className="h-screen w-full snap-y snap-mandatory overflow-x-hidden overflow-y-scroll scroll-smooth *:snap-start">
+    <main className="h-[100dvh] w-full snap-y snap-mandatory overflow-x-hidden overflow-y-scroll scroll-smooth *:snap-start">
       <Hero />
       <About />
       <Projects />
