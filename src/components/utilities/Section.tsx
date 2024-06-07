0ec06371df124a2651d99f06c218346f4bf3f907
armangohari <arman.gohari.2001@gmail.com>
2024-06-07 11:39:57 +0330

    style: h-[100dvh] to all sections

--- src/components/utilities/Section.tsx
+++ src/components/utilities/Section.tsx
@@ -17,7 +17,7 @@ export default function Section({
   children,
 }: SectionProps) {
   return (
-    <section className="relative h-full w-full px-[5vw] sm:px-[10vw]">
+    <section className="relative h-[100dvh] w-full px-[5vw] sm:px-[10vw]">
       {/* Blurred Orb - right/left align based on `orbDirection` prop */}
       <div
         className={cn(
