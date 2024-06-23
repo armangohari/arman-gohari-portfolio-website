4cbea6f6f9a6c53695fc19af9913aa7c9237f99b
armangohari <arman.gohari.2001@gmail.com>
2024-06-23 10:58:11 +0330

    refactor: change the size of the hero orb

--- src/components/utilities/HeroOrb.tsx
+++ src/components/utilities/HeroOrb.tsx
@@ -1,3 +1,3 @@
 export default function HeroOrb() {
-  return <div className="h-[300px] w-[300px] bg-primary blur-[200px] rounded-full" />;
+  return <div className="h-[250px] w-[250px] bg-primary blur-[200px] rounded-full" />;
 }
