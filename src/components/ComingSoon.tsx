add2b077cf6dc5eb1f6344a28beaeb295f2d5dd1
armangohari <arman.gohari.2001@gmail.com>
2024-06-26 13:36:34 +0330

    fix: remove cog icon from coming soon

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,4 +1,3 @@
-import { cogGif } from "@/lib/icons";
 import Image from "next/image";
 
 export default function ComingSoon() {
@@ -6,8 +5,6 @@ export default function ComingSoon() {
     <div className="relative grid place-items-center overflow-hidden rounded-3xl bg-black p-px">
       {/* Loading Card Inner Container */}
       <div className="relative z-10 flex animate-pulse items-center justify-center gap-1.5 rounded-3xl bg-black px-6 py-6">
-        {/* Icon */}
-        <Image src={cogGif} alt="cog" className="h-7 w-7" />
         {/* Loading Text */}
         <h2 className="text-3xl tracking-wider">Coming Soon</h2>
       </div>
