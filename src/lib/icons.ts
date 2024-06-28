da3ee22a5286bd07992c9dbc05a31b5aa4687b2f
armangohari <arman.gohari.2001@gmail.com>
2024-06-28 19:46:26 +0330

    feat: add icons for each interest stack section

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -1,3 +1,4 @@
+/* Contact Icons */
 import github from "../../public/assets/icons/github.svg";
 import instagram from "../../public/assets/icons/instagram.svg";
 import linkedin from "../../public/assets/icons/linkedin.svg";
@@ -6,3 +7,11 @@ import telegram from "../../public/assets/icons/telegram.svg";
 import twitter from "../../public/assets/icons/twitter.svg";
 
 export { github, instagram, linkedin, mail, telegram, twitter };
+
+/* Interests Icons */
+import cpu from "../../public/assets/icons/cpu.svg";
+import layers from "../../public/assets/icons/layers.svg";
+import market from "../../public/assets/icons/market.svg";
+import paint from "../../public/assets/icons/paint.svg";
+
+export { cpu, layers, market, paint };
